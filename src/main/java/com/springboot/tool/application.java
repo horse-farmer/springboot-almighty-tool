3d8cc91738fa458102df711307fe22e5909a65e1
horse-farmer <574441672@qq.com>
2020-12-30 16:47:49 +0800

    第一次创建

---
+++ src/main/java/com/springboot/tool/application.java
@@ -0,0 +1,12 @@
+package com.springboot.tool;
+
+/**
+ * @Description 启动类
+ * @Author horse farmer
+ * @Date 2020/12/30 12:46
+ */
+public class application {
+    public static void main(String[] args) {
+
+    }
+}
