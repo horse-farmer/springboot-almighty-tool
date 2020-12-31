360ac2da92c6554ddbaf42f01b579a69d6936968
horse-farmer <574441672@qq.com>
2020-12-31 15:20:59 +0800

    第一次创建，添加依赖

---
+++ src/main/java/com/springboot/tool/Application.java
@@ -0,0 +1,16 @@
+package com.springboot.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 启动类
+ * @Author horse farmer
+ * @Date 2020/12/30 12:46
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
